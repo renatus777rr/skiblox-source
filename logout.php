a12cf6f2c4ba8828a226c3322ecf775cc33c886b
renatus777rr <kovskirenatus1@gmail.com>
2026-05-29 07:04:50 -0700

    Refactor logout logic to use common functions

--- logout.php
+++ logout.php
@@ -1,16 +1,5 @@
 <?php
-// logout.php
 declare(strict_types=1);
-if (session_status() === PHP_SESSION_NONE) {
-    session_name('SKIBLOXSESSID');
-    session_start();
-}
-$_SESSION = [];
-setcookie(session_name(), '', time() - 3600, '/');
-if (ini_get('session.use_cookies')) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 3600, $params['path'] ?? '/', $params['domain'] ?? '', $params['secure'] ?? false, $params['httponly'] ?? true);
-}
-session_destroy();
-header('Location: /login');
-exit;
+require_once __DIR__ . '/common.php';
+logout_user();
+redirect('/login');
